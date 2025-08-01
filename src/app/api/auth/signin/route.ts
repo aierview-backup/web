85a49ae210f940d501fd14ec821f2bbd56a17185
gervasioartur <gervasioartur@outlook.com.br>
2025-08-01 17:14:40 -0300

    addin local auth route

---
+++ src/app/api/auth/signin/route.ts
@@ -0,0 +1,24 @@
+import HttpClient from "@/shared/utils/HttpClient";
+import { NEXT_PUBLIC_API_URL } from "@/shared/utils/lib";
+import { serialize } from "cookie";
+import { NextRequest, NextResponse } from "next/server";
+
+const externalHttp = HttpClient.getInstance(NEXT_PUBLIC_API_URL);
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const { user } = body;
+
+  const userCookie = serialize("user", JSON.stringify(user), {
+    httpOnly: true,
+    secure: true,
+    // secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    path: "/",
+    // maxAge: 60 * 60 * 24 * 7,
+  });
+
+  const res = NextResponse.json({ success: true });
+  res.headers.append("Set-Cookie", userCookie);
+  return res;
+}
--- src/shared/services/impl/auth.service.ts
+++ src/shared/services/impl/auth.service.ts
@@ -2,12 +2,17 @@ import IAuthService from "@/shared/services/contract/IAuthService";
 import { GoogleSinginType, SigninType, SignupType } from "@/shared/types";
 import HttpClient from "@/shared/utils/HttpClient";
 import { NEXT_PUBLIC_API_URL } from "@/shared/utils/lib";
+import UserService from "./user.service";
 
 export default class AuthService implements IAuthService {
+  private readonly localHttp = HttpClient.getInstance();
   private readonly externalHttp = HttpClient.getInstance(NEXT_PUBLIC_API_URL);
+  private readonly userService = new UserService();
 
   async googleSingin(params: GoogleSinginType): Promise<void> {
     await this.externalHttp.post(`/auth/external/google`, params);
+    const user = await this.userService.getUserDetails();
+    await this.localHttp.post(`/api/auth/signin`, user);
   }
 
   async signup(params: SignupType): Promise<void> {
